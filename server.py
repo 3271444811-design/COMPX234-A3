35c389af24614b0c1da05ef938e29eb312903c61
Jia wei Li <3271444811@qq.com>
2026-04-22 21:58:30 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+def recv_all(conn, n):
+    
+    data = b''
+    while len(data) < n:
+        chunk = conn.recv(n - len(data))
+        if not chunk:
+            return None
+        data += chunk
+    return data
+
+def main():
+    port = int(sys.argv[1])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', port))
+    s.listen(10)
+    print(f"Server on port {port}")
+    
+    
